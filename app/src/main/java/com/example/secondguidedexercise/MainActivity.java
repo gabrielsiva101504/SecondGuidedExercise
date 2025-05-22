62bed02717a3ba14b01ad05e500505f67fbdd74a
gabriel <gabriel.siva101504@gmail.com>
2025-05-22 21:43:29 +0800

    created SecondGuidedExercise

--- app/src/main/java/com/example/secondguidedexercise/MainActivity.java
+++ app/src/main/java/com/example/secondguidedexercise/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.secondguidedexercise;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNameGE2;
+    Button btnClickGE2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etNameGE2 = findViewById(R.id.etNameGE2);
+        btnClickGE2 = findViewById(R.id.btnClickGE2);
+
+        btnClickGE2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etNameGE2.getText().toString();
+                Toast.makeText(MainActivity.this,
+                        "Hello " + name + "\nWelcome to Android Development",
+                        Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
